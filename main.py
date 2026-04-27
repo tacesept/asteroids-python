ffba0aaa9af8a1c457ad7ce71147d8a7d9c8d2fd
thawit <155454715+tacesept@users.noreply.github.com>
2026-04-27 12:14:16 +0700

    Add shooting mechanics and shot class
    - Introduced SHOT_RADIUS and PLAYER_SHOOT_SPEED constants in constants.py.
    - Created Shot class in shot.py to handle shot creation and rendering.
    - Updated Player class in player.py to include shooting functionality triggered by the spacebar.
    - Modified main.py to manage shot instances within the game loop.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -16,8 +17,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 

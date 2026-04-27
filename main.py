ebd807acec554e8bc9f70bfafe9085d5d657968d
thawit <155454715+tacesept@users.noreply.github.com>
2026-04-27 12:08:18 +0700

    Enhance CircleShape and main.py for collision detection and event logging
    - Updated .gitignore to exclude game_events.jsonl.
    - Added collides_with method in CircleShape class to handle collision detection.
    - Modified main.py to log player hit events and terminate the game upon collision with asteroids.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
+
 import pygame
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -33,6 +35,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
